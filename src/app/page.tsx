252a64fff12997da25574411a8e9933c485e884a
Chad Bumstead <cbumstead@sureco.com>
2025-05-03 12:21:05 -0700

    add key to advocate search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.firstName + advocate.lastName}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
